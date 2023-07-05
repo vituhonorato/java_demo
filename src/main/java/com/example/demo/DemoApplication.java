c3f6445a860f99a05aae89f06fd4d321a7fc6910
vituhonorato <vituhonorato.89@gmail.com>
2023-07-05 16:19:17 -0300

    create StudentController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,36 +1,21 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+
+
+
 
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				new Student(
-						1L,
-						"Victor",
 
-						LocalDate.of(1989, Month.AUGUST,8),
-						"victor@gmail.com",
-						33
-
-				)
-		);
-	}
 
 }
